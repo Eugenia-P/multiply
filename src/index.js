acf9e74f88339d475a0b1c8662c910c2643450d0
Eugenia-P <eugenia.protasevich@yandex.by>
2019-10-12 15:14:49 +0300

    feat: my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+  //BigInt is a new built-in object which can be used for arbitrarily large integers
+  //Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt
+  
+  return (BigInt(first) * BigInt(second)).toString(); 
+};
+
+
